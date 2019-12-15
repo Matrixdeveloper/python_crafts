f1b4d48c7b96e0be031fa590e1823a29476df4e5
narutu <yuw857@mail.usask.ca>
2019-12-15 13:45:42 -0600

    Initial commit

---
+++ warm_up_dir/hello_world.py
@@ -0,0 +1,15 @@
+# FakeCode 2019
+
+import math as m
+
+print("Hello world!")
+print("Let's do some calculation")
+
+a = 2
+b = 10
+
+print("2 to the 10 is : >"+ str(m.pow(2,10)))
+print("2 to the 20 is : >"+ str(m.pow(2,20)))
+print("2 to the 30 is : >"+ str(m.pow(2,30)))
+
+print("End line")
